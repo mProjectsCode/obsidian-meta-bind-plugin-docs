c5e6614a3b314d0f4fe85af5d7d2db10ce8926ea
mProjectsCode <m.projects.code@gmail.com>
2022-07-02 00:37:38 +0200

    404 handling

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import {createRouter, createWebHistory} from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import AboutView from '@/views/AboutView.vue';
 import TestView from '@/views/TestView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -31,6 +32,10 @@ const router = createRouter({
             path: '/test',
             name: 'test',
             component: TestView,
+        },
+        {
+            path: '/:catchAll(.*)',
+            component: NotFoundView,
         },
     ],
 });
